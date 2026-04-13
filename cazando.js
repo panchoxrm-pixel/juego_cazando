268d7811186eb5a6e2ad082d216d99d836784268
Francisco Rodríguez <panchoxrm@gmail.com>
2026-04-12 23:41:16 -0500

    feat: Crear variables y constantes del gato y la comida

--- cazando.js
+++ cazando.js
@@ -1,7 +1,27 @@
 let canvas=document.getElementById("areaJuego");
 let ctx=canvas.getContext("2d");
 
+const ANCHO_GATO=200;
+const ALTO_GATO=90;
+const ANCHO_COMIDA=20;
+const ALTO_COMIDA=20;
+
+let gatoX=(canvas.width/2)-(ANCHO_GATO/2);
+let gatoY=(canvas.height/2)-(ALTO_GATO/2);
+let comidaX=canvas.width-ANCHO_COMIDA;
+let comidaY=canvas.height-ALTO_COMIDA;
+
 function graficarGato(){
     ctx.fillStyle = "#F2EEED"
-    ctx.fillRect(canvas.width/4,canvas.height/4,canvas.width*2/4,canvas.height*2/4);
+    ctx.fillRect(gatoX,gatoY,ANCHO_GATO,ALTO_GATO);
+}
+
+function graficarComida(){
+    ctx.fillStyle ="#27b83f";
+    ctx.fillRect(comidaX,comidaY,ANCHO_COMIDA,ALTO_COMIDA);
+}
+
+function iniciarJuego(){
+    graficarGato();
+    graficarComida();
 }
